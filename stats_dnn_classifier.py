87a945e33bbbb1148228cd70c53bbbdd0d57c6b7
Edward Chow <slyfox_chow@foxmail.com>
2020-02-26 20:53:05 +0800

    Optimize the code

--- stats_dnn_classifier.py
+++ stats_dnn_classifier.py
@@ -1,7 +1,4 @@
 # coding='UTF-8'
-import io
-import sys
-import os
 import re
 import pandas as pd
 import nltk
@@ -23,7 +20,6 @@ import matplotlib.pyplot as plt
 
 from evaluation_metrics import  precision, recall, fbeta_score, fmeasure, getAccuracy
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')  #解决print输出报编码错问题
 
 def prepare_input(file_num_limit):
     # 仅统计数据部分不需要进行文件内容读取
@@ -32,8 +28,8 @@ def prepare_input(file_num_limit):
 
     # para_encoded_txts = os.listdir(sampled_para_encoded_dir)
     sampled_label_data = pd.read_csv(sampled_para_txt_list, encoding='utf-8-sig')
-    onehotlabels = sampled_label_data.iloc[:file_num_limit,2:8]
-    stats_features = sampled_label_data.iloc[:file_num_limit,10:]
+    onehotlabels = sampled_label_data.iloc[:file_num_limit,2:8].values
+    stats_features = sampled_label_data.iloc[:file_num_limit,10:].values
 
     return onehotlabels, stats_features
 
@@ -41,8 +37,7 @@ def prepare_input(file_num_limit):
 def DNN_stats(X_train_stats, y_train, X_val_stats, y_val, batch_size, epochs):
     # stats part
     stats_input = Input(shape=(65,), name='stats_input')
-    concat_layer = concatenate([content_feedforward, stats_input])
-    x = Dense(128, activation='relu', name='merged_feedforward_1')(concat_layer)
+    x = Dense(128, activation='relu', name='merged_feedforward_1')(stats_input)
     x = Dense(64, activation='relu', name='merged_feedforward_2')(x)
     possibility_outputs = Dense(1, activation='sigmoid', name='label_output', kernel_regularizer=regularizers.l2(0.01))(x)  # softmax  sigmoid
     
@@ -71,9 +66,9 @@ if __name__ == '__main__':
     # no_footnotes-0, primary_sources-1, refimprove-2, original_research-3, advert-4, notability-5
     no_good_flaw_type = 0 # finished
     # 找出FA类的索引
-    FA_indexs = [index for index in range(len(onehotlabels)) if sum([int(item) for item in onehotlabels[index]]) == 0]
+    FA_indexs = [index for index in range(len(onehotlabels)) if sum([int(item) for item in onehotlabels[index, :]]) == 0]
     # 找出二分类另外一类的索引
-    not_good_indexs = [index for index in range(len(onehotlabels)) if onehotlabels[index][no_good_flaw_type] > 0]
+    not_good_indexs = [index for index in range(len(onehotlabels)) if onehotlabels[index,no_good_flaw_type] > 0]
     binary_classification_indexs = FA_indexs + not_good_indexs
     print('FA count:', len(FA_indexs), 'no good count:', len(not_good_indexs))
     onehotlabels = np.array([onehotlabels[index] for index in binary_classification_indexs])
@@ -104,7 +99,7 @@ if __name__ == '__main__':
         # X_val, y_val = X_train[-1000:], y_train[-1000:]
         # X_train, y_train = X_train[:-1000], y_train[:-1000]
         model, history = DNN_stats(X_train_stats_kfold, y_train_kfold, X_val_stats_kfold, y_val_kfold, batch_size, epochs)
-        prediction = model.predict(X_test_content_kfold)  # {'content_bert_input': X_test_content, 'stats_input': X_test_stats}
+        prediction = model.predict(X_test_stats_kfold)  # {'content_bert_input': X_test_content, 'stats_input': X_test_stats}
         _precision, _recall, _f1_score = getAccuracy(prediction, y_test_kfold)
         print('precision:', _precision, 'recall', _recall, 'f1_score', _f1_score)
         kfold_accuracy.append(_precision)
